b6ff5856598485659e5aa86f09406873e9b28b07
Kohei Yoshida <was914hs@gmail.com>
2026-04-27 21:42:13 +0900

    functionを追加

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 window.addEventListener('touchmove', handleTouch, { passive: true});
 const navToggle = document.getElementById('nav-toggle');
 
-navToggle.addEventListener('change',() => {
+function handleTouch(e) {
+    if(navToggle.checked) {
+        e.preventDefault();
+    }
+}
+
+window.addEventListener('touchmove',handleTouch,{passive: false});
+
+navToggle.addEventListener('change', () => {
     if (navToggle.checked) {
         document.body.classList.add('is-active');
     }else {
